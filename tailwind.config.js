2e6e4a53070c301ac695307518816efbe2924e0b
Kimmo <39192814+bithsnake@users.noreply.github.com>
2022-11-02 03:53:05 +0100

    WIP UPDATE AGAIN

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screen: {
+      '2xl': '1536px',
+    },
     extend: {},
   },
+  colors: {
+    primary : '#0101010'
+  },
   variants: {
-    extend: {},
+    extend: {
+      // backgroundColor: ['active'],
+      // // ...
+      // borderColor: ['focus-visible', 'first'],
+      // // ...
+      // textColor: ['visited'],
+    },
   },
   plugins: [],
 }
